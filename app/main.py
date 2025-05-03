32fab992c9267cff51017e15438ac1b6bf3ad0e4
sushantksah <sushantksah@gmail.com>
2025-05-03 18:19:38 -0400

    FastAPI Instance/Converter

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-#test
+from fastapi import FastAPI
+from app.exchange import covert_currency
+
+# Create a FASTAPI INSTANCE 
+app = FastAPI()
+
+# FastAPI handles JSON serialization/deserialization 
+# use built-in python and pydantic types 
+@app.get("/convert")
+def convert(from_currency: str, to_currency: str, amount: float):
+    result = convert_currency(from_currency, to_currency, amount)
+    return{"converted_amount": result}
+
+    
